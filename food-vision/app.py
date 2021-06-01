fcdc3a1f9b4eacbe329df53f6c16d48dc40ba89f
gauravreddy08 <gauravreddy008@gmail.com>
2021-06-01 13:18:50 +0530

    Updated app.py

--- food-vision/app.py
+++ food-vision/app.py
@@ -28,7 +28,20 @@ st.set_page_config(page_title="Food Vision")
 
 #### SideBar ####
 
-choosen_model = st.sidebar.selectbox("Select an Model", ('EfficientNetB0', 'ResNetV2', 'MobileNet'))
+st.sidebar.title("What's Food Vision ?")
+st.sidebar.write("""
+FoodVision is an end-to-end **CNN Image Classification Model** which identifies the food in your image. 
+
+It can identify over 100 different food classes
+
+It is based upom a pre-trained Image Classification Model that comes with Keras and then retrained on the infamous **Food101 Dataset**.
+
+**Accuracy :** **`78.4%`**
+
+**Model :** **`EfficientNetB0`**
+
+**Dataset :** **`Food101`**
+""")
 
 
 #### Main Body ####
@@ -39,21 +52,17 @@ st.write("To know more about this app, visit [**GitHub**](https://github.com/gau
 file = st.file_uploader(label="Upload an image of food.",
                         type=["jpg", "jpeg", "png"])
 
-if choosen_model == "EfficientNetB0":
-    model = tf.keras.models.load_model("./models/EfficientNetB0.hdf5")
 
-elif choosen_model == "ResNetV2":
-    model = tf.keras.models.load_model("./models/EfficientNetB0.hdf5")
-else:
-    model = tf.keras.models.load_model("./models/EfficientNetB0.hdf5")
+model = tf.keras.models.load_model("./models/EfficientNetB0.hdf5")
+
 
 st.sidebar.markdown("Created by **Gaurav Reddy**")
 st.sidebar.markdown(body="""
  <tr>
- <th><a href="https://twitter.com/gaurxvreddy" target="blank"><img align="center" src="https://bit.ly/3wK17I6" alt="gaurxvreddy" height="40" width="40" /></a></th>
- <th><a href="https://linkedin.com/in/gauravreddy08" target="blank"><img align="center" src="https://bit.ly/3wCl82U" alt="gauravreddy08" height="40" width="40" /></a></th>
- <th><a href="https://github.com/gauravreddy08" target="blank"><img align="center" src="https://bit.ly/3c2onZS" alt="16034820" height="40" width="40" /></a></th>
- <th><a href="https://instagram.com/gaurxv_reddy" target="blank"><img align="center" src="https://bit.ly/3oZABHZ" alt="gaurxv_reddy" height="40" width="40" /></a> </th>
+ <th style="border: none; background-color: rgba(0, 0, 0, 0);"><a href="https://twitter.com/gaurxvreddy" target="blank"><img align="center" src="https://bit.ly/3wK17I6" alt="gaurxvreddy" height="40" width="40" /></a></th>
+ <th style="border: none; background-color: rgba(0, 0, 0, 0);"><a href="https://linkedin.com/in/gauravreddy08" target="blank"><img align="center" src="https://bit.ly/3wCl82U" alt="gauravreddy08" height="40" width="40" /></a></th>
+ <th style="border: none; background-color: rgba(0, 0, 0, 0);"><a href="https://github.com/gauravreddy08" target="blank"><img align="center" src="https://bit.ly/3c2onZS" alt="16034820" height="40" width="40" /></a></th>
+ <th style="border: none; background-color: rgba(0, 0, 0, 0);"><a href="https://instagram.com/gaurxv_reddy" target="blank"><img align="center" src="https://bit.ly/3oZABHZ" alt="gaurxv_reddy" height="40" width="40" /></a> </th>
  </tr>
 """, unsafe_allow_html=True)
 
